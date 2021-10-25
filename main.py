2057f96bca6605f95e5bb1bc26f00a2b0a13cc1d
Paulo Mujuru <paulomujuru@Paulos-MacBook-Pro.local>
2021-10-25 13:04:23 +0200

    init

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.schema import UniqueConstraint
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+CORS(app)
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    '''
+    The reason for the autoincrement=False is because
+    the product will be created in the admin app not main app
+    '''
+
+    id: int
+    title: str
+    image: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+
+# @dataclass
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+
+
+
+@app.route('/')
+def index():
+    return "Hello"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
